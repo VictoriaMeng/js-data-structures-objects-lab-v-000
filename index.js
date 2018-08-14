5a1d41c978b04427a04f8fda4457f243628c53af
Victoria Meng <nights1ststar@protonmail.com>
2018-08-14 07:00:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your solution in this file!
 const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value){
-  driver[key] = value;
-  return driver;
+  return {key: value, ...driver}
 }
