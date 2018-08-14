815e70f4b37bb6001d538da7f0b931fd400344c9
Victoria Meng <nights1ststar@protonmail.com>
2018-08-14 07:08:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 function deleteFromDriverByKey(driver, key) {
   const newObj = {...driver};
-  delete newObj.key;
-  return newObj
+  return delete newObj.key
 }
